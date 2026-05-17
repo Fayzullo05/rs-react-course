4aa70d0124f1e652411915585139a8cdad070337
Fayzullo05 <sharifxonovfayzullo@gmail.com>
2026-05-17 22:11:46 +0500

    feat: add detail page for items

--- src/pages/main/mainPage.tsx
+++ src/pages/main/mainPage.tsx
@@ -1,7 +1,8 @@
+import { Outlet } from 'react-router-dom';
 import Layout from '../../components/layout/layout';
 
 function MainPage() {
-  return <Layout />;
+  return <Layout detailsSlot={<Outlet />} />;
 }
 
 export default MainPage;
