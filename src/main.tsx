77f4479a4ba06312ffd0a6d64c0861d793caffb7
Fayzullo05 <sharifxonovfayzullo@gmail.com>
2026-05-22 23:12:00 +0500

    feat: set up redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
+import { store } from './store/store.ts';
 
 const rootElement = document.getElementById('root');
 
@@ -10,7 +12,9 @@ if (!rootElement) {
 }
 
 createRoot(rootElement).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
